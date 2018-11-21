2478922886d6181e4098c4e4827574d8db2d61a4
BHaponiuk <brendahaponiuk@gmail.com>
2018-11-21 14:21:39 -0300

    Primer commit

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <ul>
+        <li>Home</li>
+        <li>Registración</li>
+        <li>Login</li>
+      </ul>
+    </header>
+
+    <section>
+      @yield()
+
+
+
+    </section>
+    <footer>
+
+      <ul>
+
+      </ul>
+    </footer>
+
+  </body>
+</html>
